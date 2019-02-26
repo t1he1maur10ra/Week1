8f80f448c5f40389bce98897eaadb0c8ccd66440
t1he1maur10ra <jv1@g.toiohomai.ac.nz>
2019-02-27 10:44:45 +1300

    yay

---
+++ JakesApp/JakesApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JakesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string name;
+
+
+
+            Console.WriteLine("Please enter your age: ");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine(name + age);
+            Console.ReadLine();
+
+        }
+    }
+}
+
